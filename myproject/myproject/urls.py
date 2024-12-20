79548789d9f689b17d1c8b32923c7ae0410a608d
ewa <ewcia.rogowska@gmail.com>
2024-12-20 13:29:09 +0100

    po lab 8

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -5,4 +5,5 @@ from django.urls import include, path
 urlpatterns = [
     path('myapp/', include('myapp.urls')), 
     path('admin/', admin.site.urls),
+    path('api/', include('myapp.api_urls'),)
 ]
